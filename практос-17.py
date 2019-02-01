113eb0125ca9f43953386985160470a8b4ae486e
Leo <46826012+CassperZCPH@users.noreply.github.com>
2019-02-01 16:52:03 +0300

    Update практос-17.py

--- практос-17.py
+++ практос-17.py
@@ -6,36 +6,31 @@ class PasswordGenerate:
     password_of_numbers = ''
     password_of_simblos = ''
     password_of_letters = ''
-
-    def generateCombinationNumbers(self, numbers):
-        self.password_of_numbers = numbers
-        print(list(permutations(self.password_of_numbers)))
-        self.password_of_numbers = ''
-    
-    def generateCombinationSimbols(self, simbols):
-        self.password_of_simblos = simbols
-        print(list(permutations(self.password_of_simblos)))
-        self.password_of_simblos = ''
-
-    def generateCombinationLetters(self, letters):
-        self.password_of_letters = letters
-        print(list(permutations(self.password_of_letters)))
-        self.password_of_letters = ''
-
-    def generateAllCombination(self, str_password):
-        for i in str_password:
+    def writeToAttributes(self, nsl):
+        for i in nsl:
             if i.lower() in numbersStr:
                 self.password_of_numbers += i
             elif i.lower() in simbolsStr:
                 self.password_of_simblos += i
             else:
                 self.password_of_letters += i
-        print(list(permutations(self.password_of_numbers)))
-        print(list(permutations(self.password_of_simblos)))
-        print(list(permutations(self.password_of_letters)))
+
+    def generateCombinationNumbers(self):
+        return list(permutations(self.password_of_numbers))
+    
+    def generateCombinationSimbols(self):
+        return list(permutations(self.password_of_simblos))
+
+    def generateCombinationLetters(self):
+        return list(permutations(self.password_of_letters))
+
+    def generateAllCombination(self):
+        return list(permutations(self.password_of_numbers)), list(permutations(self.password_of_simblos)), list(permutations(self.password_of_letters))
 
 myClass = PasswordGenerate()
-myClass.generateCombinationNumbers(input('Press input numbers: '))
-myClass.generateCombinationSimbols(input('Press input special simbols: '))
-myClass.generateCombinationLetters(input('Press input letters: '))
-myClass.generateAllCombination(input('Press input string of password: '))
+myClass.writeToAttributes(input('Press input string of password: '))
+print(myClass.generateCombinationNumbers(),
+    myClass.generateCombinationSimbols(),
+    myClass.generateCombinationLetters(),
+    myClass.generateAllCombination()
+)
